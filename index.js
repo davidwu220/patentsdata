c820d63a6dfab280cd7d621cc6182670b38cd034
David <info.patentsdata@gmail.com>
2017-04-25 16:28:19 -0700

    last test

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/cool', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port::::', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
